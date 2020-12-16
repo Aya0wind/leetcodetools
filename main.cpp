ff5bad5550dcfff441848c017c26d6bd90ebfbcc
li <120035959@qq.com>
2020-12-16 14:27:56 +0800

    changed utility

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
-#include <array>
-#include <string_view>
-#include <numeric>
 #include "utility.hpp"
 #include <algorithm>
+#include <numeric>
+using std::vector;
+using std::string;
+using std::hash;
+using std::array;
+using std::accumulate;
+using std::unordered_map;
+using std::operator""s;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(const vector<string>& strs) {
+        // 自定义对 array<int, 26> 类型的哈希函数
+        auto arrayHash = [] (const array<int, 26>& arr) -> size_t {
+            return accumulate(arr.begin(), arr.end(), 0u, [&](size_t acc, int num) {
+                return (acc << 1) ^ hash<int>()(num);
+            });
+        };
+
+        unordered_map<array<int, 26>, vector<string>, decltype(arrayHash)> mp(0, arrayHash);
+        for (auto& str: strs) {
+            array<int, 26> counts{};
+            int length = str.length();
+            for (int i = 0; i < length; ++i) {
+                counts[str[i] - 'a'] ++;
+            }
+            mp[counts].emplace_back(str);
+        }
+        vector<vector<string>> ans;
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            ans.emplace_back(it->second);
+        }
+        return ans;
+    }
+};
+
 
 int main(){
-    auto vec=std::vector{1,2,3,4,5};
-    std::next_permutation(vec.begin(),vec.end());
+    Solution a;
+    tools::binTree b("[1,3,4,2,5,6,6]");
+    auto list=tools::LinkedList({1,2,3,4,2,5});
+    tools::print(list);
 }
--- utility.hpp
+++ utility.hpp
@@ -1,9 +1,5 @@
-//
-// Created by 12003 on 2020/8/12.
-//
-
-#ifndef LEETCODECPP_UTILITY_HPP
-#define LEETCODECPP_UTILITY_HPP
+#pragma once
+#include <cmath>
 #include <concepts>
 #include <iostream>
 #include <queue>
@@ -11,10 +7,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <type_traits>
 
 namespace tools {
 
-using namespace std;
 struct ListNode {
     int val;
     ListNode* next;
@@ -25,41 +21,55 @@ struct ListNode {
     }
 };
 
-[[maybe_unused]] ListNode* createListNode(const std::initializer_list<int>& list)
-{
-    vector num(list);
-    if (num.empty())
-        return nullptr;
-    else {
-        auto* head = new ListNode(num[ 0 ]);
+class LinkedList{
+    ListNode* head;
+
+public:
+    LinkedList(const std::initializer_list<int>& list)
+    {
+        ::std::vector num(list);
+        if (num.empty())
+        {
+            head= nullptr;
+            return;
+        }
+        else {
+            head = new ListNode(num[ 0 ]);
+            ListNode* curNode = head;
+            for (int i = 1; i < num.size(); i++) {
+                curNode->next = new ListNode(num[ i ]);
+                curNode = curNode->next;
+            }
+        }
+    }
+
+    [[nodiscard]] std::string to_string() const
+    {
+        std::stringstream ss;
         ListNode* curNode = head;
-        for (int i = 1; i < num.size(); i++) {
-            curNode->next = new ListNode(num[ i ]);
+        while (curNode != nullptr) {
+            ss << curNode->val << " -> ";
             curNode = curNode->next;
         }
-        return head;
+        ss << "nullptr";
+        return ss.str();
     }
-}
 
-void printLinkedList(ListNode* head)
-{
-    ListNode* curNode = head;
-    while (curNode != nullptr) {
-        cout << curNode->val << " -> ";
-        curNode = curNode->next;
+    ~LinkedList()
+    {
+        ListNode* curNode = head;
+        while (curNode != nullptr) {
+            ListNode* delNode = curNode;
+            curNode = curNode->next;
+            delete delNode;
+        }
     }
-    cout << "nullptr" << endl;
-}
+    ListNode* getRoot(){
+        return this->head;
+    }
+};
 
-[[maybe_unused]] void deleteLinkedList(ListNode* head)
-{
-    ListNode* curNode = head;
-    while (curNode != nullptr) {
-        ListNode* delNode = curNode;
-        curNode = curNode->next;
-        delete delNode;
-    }
-}
+
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -85,14 +95,14 @@ private:
         delete root;
     };
 
-    static vector<int> extractIntArray(string str)
+    static ::std::vector<int> extractIntArray(::std::string str)
     {
-        vector<int> intList;
+        ::std::vector<int> intList;
         str = str.substr(1, str.size() - 2);
         str += ',';
         for (int pos = 0; pos < str.size(); pos += 1) {
             int tailPos = str.find(',', pos);
-            string intStr = str.substr(pos, tailPos - pos);
+            ::std::string intStr = str.substr(pos, tailPos - pos);
 
             if (intStr == "null")
                 intList.push_back(NULLPTR_VAR);
@@ -105,9 +115,9 @@ private:
         return intList;
     }
 
-    void setBinTreeByInt(vector<int>& list)
+    void setBinTreeByInt(::std::vector<int>& list)
     {
-        queue<TreeNode*> q;
+        ::std::queue<TreeNode*> q;
         freeBinTree();
 
         if (list.empty() || list[ 0 ] == NULLPTR_VAR)  // "[]" equal "[null]"
@@ -133,10 +143,10 @@ private:
         }
     };
 
-    [[nodiscard]] vector<int> flattenBinTree() const
+    [[nodiscard]] ::std::vector<int> flattenBinTree() const
     {
-        vector<int> flatIntList;
-        queue<TreeNode*> q;
+        ::std::vector<int> flatIntList;
+        std::queue<TreeNode*> q;
         q.push(m_root);
 
         if (!m_root)
@@ -160,14 +170,14 @@ private:
         return flatIntList;
     }
 
-    static vector<string> convertStrList(vector<int>& flatIntList)
+    static ::std::vector<::std::string> convertStrList(::std::vector<int>& flatIntList)
     {
-        vector<string> flatStrList;
+        ::std::vector<::std::string> flatStrList;
         for (auto var : flatIntList) {
             if (var == NULLPTR_VAR)
                 flatStrList.emplace_back("null");
             else
-                flatStrList.push_back(to_string(var));
+                flatStrList.push_back(::std::to_string(var));
         }
         return flatStrList;
     }
@@ -176,24 +186,24 @@ private:
     {
         if (root == nullptr)
             return 0;
-        return max(treeHeight(root->left), treeHeight(root->right)) + 1;
+        return std::max(treeHeight(root->left), treeHeight(root->right)) + 1;
     }
 
-    void writeRootToBoard(TreeNode* root, int height, int left, int right, vector<vector<string>>& board) const
+    void writeRootToBoard(TreeNode* root, int height, int left, int right, ::std::vector<::std::vector<::std::string>>& board) const
     {
         if (root == nullptr)
             return;
         int mid = (left + right) / 2;
-        board[ height ][ mid ] = to_string(root->val);
+        board[ height ][ mid ] = ::std::to_string(root->val);
         writeRootToBoard(root->left, height + 1, left, mid - 1, board);
         writeRootToBoard(root->right, height + 1, mid + 1, right, board);
     }
 
-    vector<vector<string>> writeTreeToBoard(TreeNode* root, vector<vector<string>>& board) const
+    [[maybe_unused]] ::std::vector<::std::vector<::std::string>> writeTreeToBoard(TreeNode* root, ::std::vector<::std::vector<::std::string>>& board) const
     {
         int height = treeHeight(root);
-        board = vector<vector<string>>(height, vector<string>(pow(2, height) - 1, ""));
-        writeRootToBoard(root, 0, 0, static_cast<int>(pow(2, height) - 2), board);
+        board = ::std::vector<::std::vector<::std::string>>(height, ::std::vector<::std::string>(std::pow(2, height) - 1, ""));
+        writeRootToBoard(root, 0, 0, static_cast<int>(std::pow(2, height) - 2), board);
         return board;
     }
 
@@ -201,7 +211,7 @@ public:
     binTree()
         : m_root(nullptr){};
 
-    [[maybe_unused]] explicit binTree(const string& str)
+    [[maybe_unused]] explicit binTree(const ::std::string& str)
         : m_root(nullptr)
     {
         setBinTree(str);
@@ -216,17 +226,17 @@ public:
         m_root = nullptr;
     }
 
-    void setBinTree(const string& str)
+    void setBinTree(const ::std::string& str)
     {
-        vector intArray = extractIntArray(str);
+        ::std::vector intArray = extractIntArray(str);
         setBinTreeByInt(intArray);
     }
 
-    [[nodiscard]] string flatBinTreeToStr() const
+    [[nodiscard]] ::std::string flatBinTreeToStr() const
     {
-        string flatStr = "[";
-        vector<int> flatIntList = flattenBinTree();
-        vector<string> flatStrList = convertStrList(flatIntList);
+        ::std::string flatStr = "[";
+        ::std::vector<int> flatIntList = flattenBinTree();
+        ::std::vector<::std::string> flatStrList = convertStrList(flatIntList);
         if (flatStrList.empty())
             return "[]";
         for (auto&& str : flatStrList){
@@ -237,18 +247,10 @@ public:
         return flatStr;
     }
 
-    [[maybe_unused]] void printTreeToScreen() const
-    {
-        vector<vector<string>> board;
-        writeTreeToBoard(m_root, board);
-        for (auto& i : board) {
-            for (auto& j : i) {
-                cout << '\t' << j;
-            }
-            cout << "\n\n";
-        }
+    [[maybe_unused]] [[nodiscard]] ::std::string to_string() const{
+        return this->flatBinTreeToStr();
     }
-
+    
     [[maybe_unused]] TreeNode* getTree()
     {
         return m_root;
@@ -258,51 +260,65 @@ public:
     }
 
 };
+
+
+
+
+
 template <class T>
 concept fundamental = ::std::is_fundamental_v<std::remove_cvref_t<T>>;
 
 
 
-template <class T>
+
+
+
+template<class T>
+concept ToString=requires{
+    {std::declval<T>().to_string()}->std::convertible_to<std::string>;
+};
+
+template<class T>
+concept Container=requires{
+    std::ranges::input_range<T>&&std::convertible_to<T,::std::string>;
+};
+
+template <class T,class value=typename std::remove_cvref<T>::type>
 concept Printable = requires{
-    fundamental<T>;
+    Container<value>||fundamental<value>||std::convertible_to<value,::std::string>||ToString<value>;
 };
 
 
 
 
-
-
-
-
 class Printer{
-    using utf8String=std::basic_string_view<char>;
-    utf8String separatorChar;
-    utf8String leftBraceChar;
-    utf8String rightBraceChar;
+    using utf8string=std::basic_string_view<char>;
+    utf8string separatorChar;
+    utf8string leftBraceChar;
+    utf8string rightBraceChar;
 public:
     constexpr Printer():separatorChar(","),leftBraceChar("["),rightBraceChar("]"){}
-    [[nodiscard]] const utf8String& getSeparatorChar() const
+    [[nodiscard]] const utf8string& getSeparatorChar() const
     {
         return separatorChar;
     }
-    void setSeparatorChar(const utf8String& separatorChar)
+    void setSeparatorChar(const utf8string& separatorChar)
     {
         Printer::separatorChar = separatorChar;
     }
-    [[nodiscard]] const utf8String& getLeftBraceChar() const
+    [[nodiscard]] const utf8string& getLeftBraceChar() const
     {
         return leftBraceChar;
     }
-    void setLeftBraceChar(const utf8String& leftBraceChar)
+    void setLeftBraceChar(const utf8string& leftBraceChar)
     {
         Printer::leftBraceChar = leftBraceChar;
     }
-    [[nodiscard]] const utf8String& getRightBraceChar() const
+    [[nodiscard]] const utf8string& getRightBraceChar() const
     {
         return rightBraceChar;
     }
-    void setRightBraceChar(const utf8String& rightBraceChar)
+    void setRightBraceChar(const utf8string& rightBraceChar)
     {
         Printer::rightBraceChar = rightBraceChar;
     }
@@ -313,68 +329,65 @@ public:
 
 template <class Range>
 requires ::std::ranges::input_range<Range>
-    std::string serilizeString(Range&& c,const Printer& printer)
-{
-    std::stringstream stream;
-    stream << printer.getLeftBraceChar();
-    for (auto&& e : c) {
-        if constexpr (fundamental<::std::ranges::range_value_t<Range>>) {
-            stream << e << printer.getSeparatorChar();
-        }
-        else {
-            stream << serilizeString(e) << printer.getSeparatorChar() << '\n';
-        }
-    }
-    stream << '\b' << printer.getRightBraceChar();
-    return stream.str();
-}
-
-
-
-template <class Range>
-requires ::std::ranges::input_range<Range>
-    [[maybe_unused]] void print(Range&& r,const Printer& printer=defaultPrinter,std::ostream& os=std::cerr){
+    [[maybe_unused]] ::std::string format(Range&& r,int deep,const Printer& printer=defaultPrinter){
     using value_type = ::std::ranges::range_value_t<std::remove_cvref_t<Range>>;
     auto separatorChar=printer.getSeparatorChar();
     auto leftBraceChar=printer.getLeftBraceChar();
     auto rightBraceChar=printer.getRightBraceChar();
+    ::std::stringstream os;
     if constexpr (fundamental<value_type>) {
-        os << leftBraceChar;
-    }
-    else {
-        os << leftBraceChar;
-    }
-    if constexpr (fundamental<value_type>) {
+        os<<leftBraceChar;
         for (auto&& e : r) {
             os << e << separatorChar;
         }
-
-    } else {
+        os<<'\b'<<rightBraceChar;
+    }else if constexpr (std::convertible_to<value_type,std::string>||std::convertible_to<value_type,std::string_view>){
+        os<<leftBraceChar;
         for (auto&& e : r) {
-            os<<'\n'<<"    ";
-            os << serilizeString(e,printer) << separatorChar << '\n';
+            os <<'\"'<< e<<'\"'<< separatorChar;
         }
+        os<<'\b'<<rightBraceChar;
     }
-    for(auto&&e:separatorChar){
-        os << '\b';
+    else if constexpr (ToString<value_type>){
+        os<<leftBraceChar;
+        for (auto&& e : r) {
+            os << e.to_string() << separatorChar;
+        }
+        os<<'\b'<<rightBraceChar;
+    }else if constexpr (Container<value_type>) {
+        os << leftBraceChar << '\n';
+        for (auto&& e : r) {
+            for (int i=0;i<deep;++i){
+                os<< "    ";
+            }
+            os << format(e,deep+1,printer) << separatorChar << '\n';
+        }
+        os << rightBraceChar;
     }
-    os<< rightBraceChar<<'\n';
-}
-template <fundamental T>
-[[maybe_unused]] void print(T t)
-{
-    std::cout<<t<<'\n';
+    return os.str();
 }
 
-[[maybe_unused]] void print(ListNode* root)
-{
-    printLinkedList(root);
+template <Printable T>
+void format_print(const T& object,std::ostream& os=std::cerr){
+    using value=typename std::remove_cvref<T>::type;
+    constexpr bool is_std_printable=std::convertible_to<value,::std::string>||std::convertible_to<::std::string_view,value>;
+    if constexpr (is_std_printable||fundamental<value>){
+        os<<object<<'\n';
+    }
+    else if constexpr (ToString<value>){
+        os<<object.to_string()<<'\n';
+    }
+    else{
+
+    }
 }
 
-[[maybe_unused]] void print(const binTree& root)
+template <Printable T>
+[[maybe_unused]] void print(const T& t)
 {
-    std::cout << root.flatBinTreeToStr();
+    format_print(t,std::cerr);
 }
 
-#endif  // LEETCODECPP_UTILITY_HPP
+
+
 }
