d57d9f9e3fff3c358c1717ceadf7a327c02ba966
Aya0wind <120035959@qq.com>
2021-06-12 12:34:18 +0800

    update

---
+++ build/CMakeFiles/leetcodeCPP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcodeCPP.dir/src/main.cpp.obj"
+  "CMakeFiles/leetcodeCPP.dir/src/main.cpp.obj.d"
+  "leetcodeCPP.exe"
+  "leetcodeCPP.exe.manifest"
+  "leetcodeCPP.lib"
+  "leetcodeCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcodeCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
